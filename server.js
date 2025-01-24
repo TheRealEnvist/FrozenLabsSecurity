ec65d190a07fd3c7b6baf21ad7f54e8020dc6213
TheRealEnvist <137974409+TheRealEnvist@users.noreply.github.com>
2025-01-23 21:03:39 -0500

    Fixed

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get('/universe/:UniverseID/information', async (req, res) => {
   const { UniverseID } = req.params;
 
 
-  res.status(200).json({RootPlace: await fetchWithRetry(`https://games.roblox.com/v1/games?universeIds=${UniverseID}`)["data"][0], Media: await fetchWithRetry(`https://games.roblox.com/v2/games/${UniverseID}/media?fetchAllExperienceRelatedMedia=false`)});
+  res.status(200).json({RootPlace: await fetchWithRetry(`https://games.roblox.com/v1/games?universeIds=${UniverseID}`)["data"], Media: await fetchWithRetry(`https://games.roblox.com/v2/games/${UniverseID}/media?fetchAllExperienceRelatedMedia=false`)});
 });
 
 app.get('/profile/:code/register', async (req, res) => {
